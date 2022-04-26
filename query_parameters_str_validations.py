7f27fbda821236ac3e58965f9db2e9af27bcd577
Fedor Chervyakov <fchervyakov@gmail.com>
2022-04-26 09:34:08 +0300

    query_parameters_str_validations: add defaults to query list

--- query_parameters_str_validations.py
+++ query_parameters_str_validations.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Optional[List[str]] = Query(None)):
+async def read_items(q: Optional[List[str]] = Query(["Foo", "Bar"])):
     query_items = {"q": q}
 
     return query_items
