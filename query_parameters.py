94de13dc27c8d9e6708d73aa8b381727fff76326
Fedor Chervyakov <fchervyakov@gmail.com>
2022-04-26 05:28:43 +0300

    query_parameters: add read_item with optional query parameter

--- query_parameters.py
+++ query_parameters.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -9,3 +11,10 @@ fake_items_db = [{"item_name": i} for i in fake_items]
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Optional[str] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
