fcda03c0c7ad04306944471f21b9b9445fb1ea4c
Fedor Chervyakov <fchervyakov@gmail.com>
2022-04-26 04:57:02 +0300

    path_parameters: add /models/{model_name} get handler with enum in path

--- path_parameters.py
+++ path_parameters.py
@@ -1,6 +1,14 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 @app.get("/")
@@ -11,3 +19,15 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
